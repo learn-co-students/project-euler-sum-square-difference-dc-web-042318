a0e90a23a0efca0aab464a5c3240287c1f8321e5
eckmLJE <lucas.eckman@gmail.com>
2018-07-27 15:12:50 -0400

    Done.

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,9 @@
 # Implement your procedural solution here!
+
+def sum_square_difference(number)
+    range = (1..number)
+    sum_of_squares = 0
+    range.each {|num| sum_of_squares += (num ** 2)}
+    square_of_sum = range.reduce(0, :+) ** 2
+    square_of_sum - sum_of_squares
+end
